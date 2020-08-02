9fae8f68a27fd7c635c8a95bc6cd6dff3fc09d68
Martin Ambrozic <martin.ambrozic@abelium.eu>
2020-08-02 10:59:05 -0700

    Minor tweaks

--- notebook/nn_code/vball_nn_predict.py
+++ notebook/nn_code/vball_nn_predict.py
@@ -10,12 +10,12 @@ import pickle
 FEATURE_COUNT = 41
 LSTM_CELL_COUNT = 256
 MAX_TRACE_LENGTH = 10
-LEARNING_RATE = 1e-5 #1e-4
+LEARNING_RATE = 1e-6 #1e-4
 BATCH_SIZE = 32 #32
 GAMMA = 1
 
 SAVE_DIR = 'C:/projects/msc-project/output'
-DATA_FILE = 'C:/projects/msc-project/data/vb_data_numZone.csv'
+DATA_FILE = 'C:/projects/msc-project/data/vb_data_3_numZone.csv'
 
 
 def train_network(sess, model, state_data, reward_data, trace_lengths):
@@ -26,13 +26,24 @@ def train_network(sess, model, state_data, reward_data, trace_lengths):
     saver = tf.train.Saver()
 
     sess.run(tf.global_variables_initializer())
+    start_iteration = 1
+
+    restore_model = True
+    if restore_model:
+        checkpoint = tf.train.get_checkpoint_state(SAVE_DIR)
+        if checkpoint and checkpoint.model_checkpoint_path:
+            start_iteration = int((checkpoint.model_checkpoint_path.split("-"))[-1]) + 1
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+            print("Successfully loaded:", checkpoint.model_checkpoint_path)
+        else:
+            print("Could not find old network weights.")
 
     # initial Q values
     [rnn_outputs_prev, q_predictions_prev] = sess.run([model.outputs, model.read_out],
                                                     feed_dict={model.trace_lengths: trace_lengths,
                                                             model.rnn_input: state_data})
 
-    for iteration in range(1,200):
+    for iteration in range(start_iteration,200):
 
         t = time.time()
         print('Iteration %d ...' % (iteration))
@@ -81,19 +92,24 @@ def train_network(sess, model, state_data, reward_data, trace_lengths):
 
         # compute mean change in Q predictions
         avg_change = np.mean(np.abs(q_predictions - q_predictions_prev))
+        avg_Q = np.mean(q_predictions)
         print('Mean change from last iteration: %.5f' % (avg_change))
+        print('Mean Q value: %.5f' % (avg_Q))
         q_predictions_prev = q_predictions
 
-        saver.save(sess, SAVE_DIR + '/nn-iter-', global_step=iteration)
+        #saver.save(sess, SAVE_DIR + '/nn-iter-', global_step=iteration)
 
         # check for convergence
-        if avg_change < 0.0005:
+        if avg_change < 0.0001:
             converge_flag = True
+        
+        if avg_change < 0.001:
+            pickle.dump(q_predictions, open('%s/q_values_%03d.pkl' % (SAVE_DIR, iteration), 'wb'))
 
         if converge_flag:
             print('Convergence detected, exiting.')
             print('Saving Q values.')
-            pickle.dump(q_predictions, open(SAVE_DIR + '/q_values.pkl', 'wb'))
+            pickle.dump(q_predictions, open(SAVE_DIR + '/q_values_new.pkl', 'wb'))
             break
 
     print('Done.')
